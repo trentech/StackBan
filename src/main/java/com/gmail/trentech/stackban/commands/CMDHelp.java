db637c7dea87f3a079688c12e58f36bb83f6051d
TrenTech <TrenTech@gmail.com>
2016-10-15 23:07:32 -0400

    fix help

--- src/main/java/com/gmail/trentech/stackban/commands/CMDHelp.java
+++ src/main/java/com/gmail/trentech/stackban/commands/CMDHelp.java
@@ -11,16 +11,16 @@ import com.gmail.trentech.stackban.utils.Help;
 public class CMDHelp implements CommandExecutor {
 
 	public CMDHelp() {
-		new Help("sban help", "help", "Get help with all commands in Project Worlds", false)
+		new Help("sban help", "help", "Get help with all commands in StackBan", false)
 			.setPermission("stackban.cmd.sban")
 			.setUsage("/sban help <rawCommand>")
-			.setExample("/sban help world create")
+			.setExample("/sban help sban create")
 			.save();
 	}
 	
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-		Help help = args.<Help> getOne("command").get();
+		Help help = args.<Help> getOne("rawCommand").get();
 		help.execute(src);
 		
 		return CommandResult.success();
